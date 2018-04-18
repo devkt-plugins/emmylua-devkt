c40d0cefec277b0d2515933aa338efa777b1b1d8
ice1000 <ice1000kotlin@foxmail.com>
2018-04-19 00:43:20 +0800

    Lexer based highlight

--- src/com/tang/intellij/devkt/lua/lua-devkt.kt
+++ src/com/tang/intellij/devkt/lua/lua-devkt.kt
@@ -13,14 +13,25 @@ class EmmyLua<T> : ExtendedDevKtLanguage<T>(
 		LuaLanguage.INSTANCE,
 		LuaParserDefinition()
 ) {
+	override fun satisfies(fileName: String) = fileName.endsWith(".lua") || fileName.endsWith(".lua.txt")
+
 	override fun attributesOf(type: IElementType, colorScheme: ColorScheme<T>) = when (type) {
 		LuaTypes.DOC_COMMENT -> colorScheme.docComments
+		LuaTypes.BLOCK_COMMENT -> colorScheme.blockComments
+		LuaTypes.SHORT_COMMENT -> colorScheme.lineComments
+		LuaTypes.STRING -> colorScheme.string
+		LuaTypes.SEMI -> colorScheme.semicolon
+		LuaTypes.COLON -> colorScheme.colon
+		LuaTypes.COMMA -> colorScheme.comma
 		LuaStringTypes.NEXT_LINE -> colorScheme.stringEscape
 		LuaTypes.LPAREN, LuaTypes.RPAREN -> colorScheme.parentheses
 		LuaTypes.LBRACK, LuaTypes.RBRACK -> colorScheme.brackets
+		LuaTypes.SHEBANG, LuaTypes.SHEBANG_LINE -> colorScheme.metaData
 		in DOC_TAG_TOKENS -> colorScheme.docComments
 		in PRIMITIVE_TYPE_SET -> colorScheme.keywords
 		in KEYWORD_TOKENS -> colorScheme.keywords
 		else -> super.attributesOf(type, colorScheme)
 	}
+
+
 }
