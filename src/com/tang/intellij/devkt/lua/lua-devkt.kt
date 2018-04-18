6da7b8470886b5575ce2f13386f412fa5884b118
ice1000 <ice1000kotlin@foxmail.com>
2018-04-19 00:54:09 +0800

    Add commenter

--- src/com/tang/intellij/devkt/lua/lua-devkt.kt
+++ src/com/tang/intellij/devkt/lua/lua-devkt.kt
@@ -15,6 +15,12 @@ class EmmyLua<T> : ExtendedDevKtLanguage<T>(
 ) {
 	override fun satisfies(fileName: String) = fileName.endsWith(".lua") || fileName.endsWith(".lua.txt")
 
+	override val blockComment: Pair<String, String>?
+		get() = "--[[" to "--]]"
+
+	override val lineCommentStart: String?
+		get() = "--"
+
 	override fun attributesOf(type: IElementType, colorScheme: ColorScheme<T>) = when (type) {
 		LuaTypes.DOC_COMMENT -> colorScheme.docComments
 		LuaTypes.BLOCK_COMMENT -> colorScheme.blockComments
@@ -23,9 +29,11 @@ class EmmyLua<T> : ExtendedDevKtLanguage<T>(
 		LuaTypes.SEMI -> colorScheme.semicolon
 		LuaTypes.COLON -> colorScheme.colon
 		LuaTypes.COMMA -> colorScheme.comma
+		LuaTypes.NUMBER -> colorScheme.numbers
 		LuaStringTypes.NEXT_LINE -> colorScheme.stringEscape
 		LuaTypes.LPAREN, LuaTypes.RPAREN -> colorScheme.parentheses
 		LuaTypes.LBRACK, LuaTypes.RBRACK -> colorScheme.brackets
+		LuaTypes.LCURLY, LuaTypes.RCURLY -> colorScheme.brackets
 		LuaTypes.SHEBANG, LuaTypes.SHEBANG_LINE -> colorScheme.metaData
 		in DOC_TAG_TOKENS -> colorScheme.docComments
 		in PRIMITIVE_TYPE_SET -> colorScheme.keywords
