3dfaf0773734599cc54c61e08c170f5f603ba700
ice1000 <ice1000kotlin@foxmail.com>
2018-04-27 00:10:03 +0800

    Update

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,42 +7,47 @@ group = "com.tang.intellij.devkt.lua"
 version = "v1.0"
 
 plugins {
-  java
-  kotlin("jvm") version "1.2.40"
+	java
+	application
+	kotlin("jvm") version "1.2.40"
 }
 
 java {
-  sourceCompatibility = JavaVersion.VERSION_1_8
-  targetCompatibility = JavaVersion.VERSION_1_8
+	sourceCompatibility = JavaVersion.VERSION_1_8
+	targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 java.sourceSets {
-  "main" {
-    java.setSrcDirs(listOf("src"))
-    withConvention(KotlinSourceSet::class) {
-      kotlin.setSrcDirs(listOf("src"))
-    }
-    resources.setSrcDirs(listOf("res"))
-  }
+	"main" {
+		java.setSrcDirs(listOf("src"))
+		withConvention(KotlinSourceSet::class) {
+			kotlin.setSrcDirs(listOf("src"))
+		}
+		resources.setSrcDirs(listOf("res"))
+	}
 
-  "test" {
-    java.setSrcDirs(emptyList<Any>())
-    withConvention(KotlinSourceSet::class) {
-      kotlin.setSrcDirs(emptyList<Any>())
-    }
-    resources.setSrcDirs(emptyList<Any>())
-  }
+	"test" {
+		java.setSrcDirs(emptyList<Any>())
+		withConvention(KotlinSourceSet::class) {
+			kotlin.setSrcDirs(emptyList<Any>())
+		}
+		resources.setSrcDirs(emptyList<Any>())
+	}
 }
 
 repositories {
-  mavenCentral()
-  jcenter()
-  maven("https://jitpack.io")
+	mavenCentral()
+	jcenter()
+	maven("https://jitpack.io")
+}
+
+application {
+	mainClassName = "org.ice1000.devkt.Main"
 }
 
 dependencies {
-  compileOnly(kotlin("compiler-embeddable", kotlinVersion))
-	val version = "efb0f52d89"
+	compileOnly(kotlin("compiler-embeddable", kotlinVersion))
+	val version = "fffe21c45d"
 	compileOnly(group = "com.github.ice1000.dev-kt", name = "common", version = version)
 	runtime(group = "com.github.ice1000.dev-kt", name = "swing", version = version)
 }
